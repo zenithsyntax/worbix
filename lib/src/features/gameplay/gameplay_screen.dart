85312ad01a84d4dbcaaeb14d1a471f4422d1b2e3
zenithsyntax <zenithsyntax.com>
2025-12-10 20:35:23 +0530

    13

--- lib/src/features/gameplay/gameplay_screen.dart
+++ lib/src/features/gameplay/gameplay_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:confetti/confetti.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'gameplay_controller.dart';
 import 'gameplay_state.dart';
 import '../ads/ad_manager.dart';
